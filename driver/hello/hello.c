3651c03f7ba4c2b7e5c39095586f6fcb7bcbae28
jeppeter <jeppeter@gmail.com>
2015-12-05 20:12:34 +0800

    to add hello driver

---
+++ driver/hello/hello.c
@@ -0,0 +1,135 @@
+#include <ntddk.h>
+#include <windef.h>
+#include <stdlib.h>
+
+/*define for driver name*/
+
+#define DEVICE_NAME L"\\Device\\HelloDrv"
+#define LINK_NAME L"\\DosDevices\\HelloDrv"
+#define LINK_GLOBAL_NAME L"\\DosDevices\\Global\\HelloDrv"
+
+#define IOCTL_IO_TEST             CTL_CODE(FILE_DEVICE_UNKNOWN,0x800,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define IOCTL_SAY_HELLO           CTL_CODE(FILE_DEVICE_UNKNOWN,0x801,METHOD_BUFFERED,FILE_ANY_ACCESS)
+
+#define DEBUG_PRINT(...)   do{DbgPrint("%s:%d\t",__FILE__,__LINE__);DbgPrint(__VA_ARGS__);}while(0)
+
+void DriverUnload(PDRIVER_OBJECT pDrvObj)
+{
+	UNICODE_STRING strLink;
+	DEBUG_PRINT("[HelloDrv]Unload\n");
+	RtlInitUnicodeString(&strLink,LINK_NAME);
+	IoDeleteSymbolicLink(&strLink);
+	IoDeleteDevice(pDrvObj->DeviceObject);
+	return;
+}
+
+NTSTATUS DispatchCreate(PDEVICE_OBJECT pDrvObj,PIRP pIrp)
+{
+	DEBUG_PRINT("[HelloDrv]DispatchCreate\n");
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp,IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DispatchClose(PDEVICE_OBJECT pDrvObj,PIRP pIrp)
+{
+	DEBUG_PRINT("[HelloDrv]DispatchClose\n");
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp,IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DispatchIoctl(PDEVICE_OBJECT pDrvObj,PIRP pIrp)
+{
+	NTSTATUS status = STATUS_INVALID_DEVICE_REQUEST;
+	PIO_STACK_LOCATION pIrpStack;
+	ULONG ctrlcode;
+	PVOID pIoBuffer;
+	ULONG insize;
+	ULONG outsize;
+
+	DEBUG_PRINT("[HelloDrv]DispatchIoctl\n");
+
+	pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+	ctrlcode = pIrpStack->Parameters.DeviceIoControl.IoControlCode;
+	pIoBuffer = pIrp->AssociatedIrp.SystemBuffer;
+	insize = pIrpStack->Parameters.DeviceIoControl.InputBufferLength;
+	outsize = pIrpStack->Parameters.DeviceIoControl.OutputBufferLength;
+
+	switch(ctrlcode)
+	{
+		case IOCTL_IO_TEST:
+		{
+			DWORD dw=0;
+			memcpy(&dw,pIoBuffer,sizeof(DWORD));
+			dw ++;
+			memcpy(pIoBuffer,&dw,sizeof(DWORD));
+			status =STATUS_SUCCESS;
+			break;
+		}
+
+		case IOCTL_SAY_HELLO:
+		{
+			DEBUG_PRINT("[HelloDrv]SayHello\n");
+			status = STATUS_SUCCESS;
+			break;
+		}
+		default:
+			break;
+	}
+
+	if (status == STATUS_SUCCESS)
+	{
+		pIrp->IoStatus.Information = outsize;
+	}
+	else
+	{
+		pIrp->IoStatus.Information = 0;
+	}
+
+	pIrp->IoStatus.Status = status;
+	IoCompleteRequest(pIrp,IO_NO_INCREMENT);
+	return status;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDrvObj,PUNICODE_STRING pRegistryString)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+	UNICODE_STRING strLink,strDevname;
+
+	PDEVICE_OBJECT pDevObj;
+
+	pDrvObj->MajorFunction[IRP_MJ_CREATE] = DispatchCreate;
+	pDrvObj->MajorFunction[IRP_MJ_CLOSE] = DispatchClose;
+	pDrvObj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DispatchIoctl;
+
+	pDrvObj->DriverUnload = DriverUnload;
+
+	RtlInitUnicodeString(&strDevname,DEVICE_NAME);
+	status = IoCreateDevice(pDrvObj,0,&strDevname,FILE_DEVICE_UNKNOWN,0,FALSE,&pDevObj);
+	if (!NT_SUCCESS(status)) 
+	{
+		return status;
+	}
+
+	if (IoIsWdmVersionAvailable(1,0x10))
+	{
+		RtlInitUnicodeString(&strLink,LINK_GLOBAL_NAME);
+	}
+	else
+	{
+		RtlInitUnicodeString(&strLink,LINK_NAME);
+	}
+
+	status = IoCreateSymbolicLink(&strLink,&strDevname);
+	if (!NT_SUCCESS(status))
+	{
+		IoDeleteDevice(pDevObj);
+		return status;
+	}
+
+	DEBUG_PRINT("[HelloDrv]DriverEntry\n");
+	return STATUS_SUCCESS;
+}
